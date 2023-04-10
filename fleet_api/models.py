98f9b167e696c0ae15fffd5b6193cd94d4bad1c7
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-04-10 13:35:30 +0300

    Set up the fleet management project - dependancies, models, serializers

---
+++ fleet_api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Aircraft(models.Model):
+    serial_no = models.CharField(max_length=100, unique=True)
+    manufacturer = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.serial_no} - {self.manufacturer}'
+
+
+class Flight(models.Model):
+    departure_airport = models.CharField(max_length=4)
+    arrival_airport = models.CharField(max_length=4)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.departure_airport} - {self.arrival_airport}'
